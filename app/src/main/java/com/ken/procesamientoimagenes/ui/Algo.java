98d0368f47917731064b26df732b19c670bbc9df
Ken <darius_dam@ciencias.unam.mx>
2015-08-24 17:45:36 -0500

    Initial commit

---
+++ app/src/main/java/com/ken/procesamientoimagenes/ui/Algo.java
@@ -0,0 +1,7 @@
+package com.ken.procesamientoimagenes.ui;
+
+/**
+ * Created by Ken on 24/08/15.
+ */
+public class Algo {
+}
---
+++ app/src/main/java/com/ken/procesamientoimagenes/util/Constans.java
@@ -0,0 +1,11 @@
+package com.ken.procesamientoimagenes.util;
+
+/**
+ * Created by Ken on 24/08/15.
+ */
+public class Constans {
+
+    public static final long DRAWER_CLOSE_DELAY_MS = 250;
+    public static final String NAV_ITEM_ID = "navItemId";
+
+}
